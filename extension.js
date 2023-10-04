6659fdddb33bc047ab428befc52e98aa3bddf8b6
sawan <sawan.ashok0@gmail.com>
2023-10-04 23:28:36 +0530

    ✨ prettier extraction code added

--- extension.js
+++ extension.js
@@ -1,32 +1,35 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const { UNNEEDED_CONTANTS } = require("./utils");
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "prettierrc-extractor" is now active!'
-  );
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
-    "prettierrc-extractor.helloWorld",
+    "prettierrc-extractor.extractPrettierrc",
     function () {
-      // The code you place here will be executed every time your command is executed
-
       // Display a message box to the user
-      vscode.window.showInformationMessage(
-        "Hello World from Prettierrc Extractor!"
+      vscode.window.showInformationMessage("Extracting Prettier Info...");
+
+      const editor = vscode.window.activeTextEditor;
+
+      // Get Prettier settings from the active workspace configuration
+      const prettierSettings = vscode.workspace.getConfiguration(
+        "prettier",
+        editor ? editor.document.uri : null
       );
+      const prettierObj = {};
+      for (const key in prettierSettings) {
+        if (UNNEEDED_CONTANTS.indexOf(key) === -1) {
+          prettierObj[key] = prettierSettings[key];
+        }
+      }
+      vscode.workspace
+        .openTextDocument({ content: JSON.stringify(prettierObj) })
+        .then((document) => {
+          vscode.window.showTextDocument(document);
+        });
     }
   );
 
