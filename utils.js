dc15dba0250a60290851cfe92a9e756e2dd4499a
sawan <sawan.ashok0@gmail.com>
2023-10-04 23:27:47 +0530

    🎉 extension setup

---
+++ utils.js
@@ -0,0 +1,22 @@
+const UNNEEDED_CONTANTS = [
+  "configPath",
+  "disableLanguages",
+  "documentSelectors",
+  "enable",
+  "enableDebugLogs",
+  "get",
+  "has",
+  "ignorePath",
+  "inspect",
+  "packageManager",
+  "prettierPath",
+  "requireConfig",
+  "update",
+  "useEditorConfig",
+  "vueIndentScriptAndStyle",
+  "withNodeModules",
+];
+
+module.exports = {
+  UNNEEDED_CONTANTS,
+};
